ea9b4dc98f3b577ad0b51827d41487d3c8e3ab99
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2025-01-21 18:01:05 -0800

    setup controllers/routes and required files

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express();
 const PORT = process.env.PORT || 5050;
 
 app.use(express.json());
+app.use(cors())
 
 app.get('/', (req, res) => {
  res.send('Basketball League Backend is Running!');
